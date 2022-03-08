aa240fcb2b18b9f01df7f861c5884fe3249e4022
Anton Zubekhin <anton-zubehin@mail.ru>
2022-03-08 15:55:35 +0300

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1,9 +1,12 @@
 import torch
 import torch.nn as nn
+import numpy as np
 from torch import optim
 import os
 from tqdm.auto import tqdm
-from torchvision.utils import save_image
+from torch.utils.tensorboard import SummaryWriter
+from torchvision.utils import save_image, make_grid
+import matplotlib.pyplot as plt
 
 from model import AE
 
@@ -32,12 +35,30 @@ def load_model(model, checkpoint_path):
     return model
 
 
+def matplotlib_imshow(img, one_channel=False):
+    if one_channel:
+        img = img.mean(dim=0)
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    if one_channel:
+        plt.imshow(npimg, cmap="Greys")
+    else:
+        plt.imshow(np.transpose(npimg, (1, 2, 0)))
+
+
+def save_images(images, config, idx):
+     pics = (images + 1) / 2
+     save_image(pics.data.cpu(),
+                os.path.join(config.output_path,
+                             '{}-images.jpg'.format(idx+1)),
+                            nrow=8)
+
+
 def train(dataloader, config):
     model = AE(config.in_channels,
                config.input_dim,
                config.out_channels,
                config.latent_dim,
-               config.slope,
                config.img_size).to(config.device)
     model.train()
 
@@ -54,6 +75,8 @@ def train(dataloader, config):
         loss_func = nn.L1Loss()
     else:
         raise NotImplementedError()
+
+    writer = SummaryWriter(os.path.join(config.base_path, 'runs'))
 
     for epoch in range(config.max_epochs):
         running_loss = 0.
@@ -77,11 +100,12 @@ def train(dataloader, config):
             if (epoch+1) % config.epoch_show == 0 and (idx+1) % config.idx_show == 0:
                 with torch.no_grad():
                     pics = torch.cat([image, reconstructed])
-                    pics = (pics + 1) / 2
-                    save_image(pics.data.cpu(),
-                               os.path.join(config.base_path, config.output_path,
-                                            '{}-images.jpg'.format(idx+1)),
-                               nrow=8)
+                    save_images(pics, config, idx)
+                    img_grid = make_grid(pics.data.cpu(), nrow=8)
+                    matplotlib_imshow(img_grid)
+                    writer.add_image('ep{epoch+1}_batch{idx+1}', img_grid)
+                    writer.flush()
+
 
         running_loss /= len(dataloader)
         print(f"[Epoch {epoch+1}], [Reconstruction loss]: {running_loss:.4f}")
