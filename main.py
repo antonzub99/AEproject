a4e39a468d0b27a5b677a596c6f62a1fe91093fa
Anton Zubekhin <anton-zubehin@mail.ru>
2022-03-08 16:59:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from build_loader import build_celeba_dataloader
 
 
 def main(config):
-    if not os.path.exists(os.path.join(config.base_path, 'weights')):
-        os.makedirs(os.path.join(config.base_path, 'weights'))
-    if not os.path.exists(os.path.join(config.base_path, 'optims')):
-        os.makedirs(os.path.join(config.base_path, 'optims'))
-    if not os.path.exists(os.path.join(config.base_path, config.output_path)):
-        os.makedirs(os.path.join(config.base_path, config.output_path))
+    if not os.path.exists(config.output_path):
+        os.makedirs(config.output_path)
+    if not os.path.exists(os.path.join(config.output_path, 'weights')):
+        os.makedirs(os.path.join(config.output_path, 'weights'))
+    if not os.path.exists(os.path.join(config.output_path, 'optims')):
+        os.makedirs(os.path.join(config.output_path, 'optims'))
 
     celeba_loader = build_celeba_dataloader(config)
     train(celeba_loader, config)
@@ -22,7 +22,8 @@ if __name__ == '__main__':
 
     parser.add_argument('--base_path', type=str, default='data')
     parser.add_argument('--output_path', type=str, 
-                        default='/content/drive/MyDrive/Colab_Notebooks/AEtraining/outputs')
+                        default='/content/drive/MyDrive/Colab_Notebooks/AEtraining')
+    parser.add_argument('--checkpoint_path', type=str, default='./')
     parser.add_argument('--in_channels', type=int, default=3)
     parser.add_argument('--out_channels', type=int, default=3)
     parser.add_argument('--input_dim', type=int, default=8)
@@ -40,13 +41,14 @@ if __name__ == '__main__':
                         choices=['mae', 'laplacian'])
     parser.add_argument('--max_epochs', type=int, default=50)
     parser.add_argument('--epoch_show', type=int, default=1)
-    parser.add_argument('--idx_show', type=int, default=5)
+    parser.add_argument('--idx_show', type=int, default=10)
     parser.add_argument('--url_dataset', type=str,
                         default=f'https://raw.githubusercontent.com/vpozdnyakov/DeepGenerativeModels/spring-2022/data/celeba/list_attr_celeba.txt')
     parser.add_argument('--attr_file', type=str,
                         default='list_attr_celeba.txt')
     parser.add_argument('--save_produced', type=bool, default=False)
-    parser.add_argument('--batch_size', type=int, default=64)
+    parser.add_argument('--resume_training', type=bool, default=False)
+    parser.add_argument('--batch_size', type=int, default=128)
 
     config = parser.parse_args()
     main(config)
