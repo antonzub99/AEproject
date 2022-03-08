22ea8c6a5f6660ad65e6449cb7c1a5eae7ccbe37
Anton Zubekhin <anton-zubehin@mail.ru>
2022-03-08 19:09:11 +0300

    updated AE architecture

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch.nn as nn
 
 
@@ -18,10 +19,10 @@ class BasicEncBlock(nn.Module):
         return self.encode(x)
 
 
+# noinspection PyTypeChecker
 class BasicDecBlock(nn.Module):
     def __init__(self, input_dim, output_dim, nonlinearity=nn.LeakyReLU(0.2)):
         super().__init__()
-        # noinspection PyTypeChecker
         block = [
             nn.Upsample(scale_factor=2, mode='nearest'),
             nn.ReplicationPad2d(1),
@@ -38,11 +39,10 @@ class BasicDecBlock(nn.Module):
 
 
 # noinspection PyTypeChecker
-class AE(nn.Module):
-    def __init__(self, in_channels, input_dim, out_channels,
+class Encoder(nn.Module):
+    def __init__(self, in_channels, input_dim,
                  latent_dim, img_size=64, num_blocks=5):
         super().__init__()
-        # noinspection PyTypeChecker
         encoder = [nn.Conv2d(in_channels, input_dim, kernel_size=1,
                              stride=1, padding=0, bias=False)]
         hidden_dim = input_dim
@@ -50,28 +50,56 @@ class AE(nn.Module):
         for _ in range(num_blocks):
             encoder.append(BasicEncBlock(hidden_dim, hidden_dim * 2))
             hidden_dim *= 2
-            spatial_size /= 2
+            spatial_size = spatial_size // 2
 
-        spatial_size = int(spatial_size)
-        final_dim = int(latent_dim / (spatial_size**2))
-
-        encoder.append(nn.Conv2d(hidden_dim, final_dim, kernel_size=1,
+        encoder.append(nn.Conv2d(hidden_dim, latent_dim, kernel_size=1,
                                  stride=1, padding=0, bias=False))
         encoder.append(nn.Flatten())
+        encoder.append(nn.Linear(latent_dim*spatial_size*spatial_size,
+                                 latent_dim, bias=True))
+        self.spatial_size = spatial_size
+        self.hidden_dim = hidden_dim
+        self.encoder = nn.Sequential(*encoder)
 
-        decoder = [nn.Unflatten(1, (final_dim, spatial_size, spatial_size)),
-                   nn.Conv2d(final_dim, hidden_dim, kernel_size=1,
+    def forward(self, x):
+        x = self.encoder(x)
+        return x
+
+
+# noinspection PyTypeChecker
+class Decoder(nn.Module):
+    def __init__(self, out_channels, input_dim, hidden_dim,
+                 latent_dim, num_blocks=5):
+        super().__init__()
+        decoder = [nn.Linear(latent_dim, latent_dim*input_dim*input_dim),
+                   nn.Unflatten(1, (latent_dim, input_dim, input_dim)),
+                   nn.Conv2d(latent_dim, hidden_dim, kernel_size=1,
                              stride=1, padding=0, bias=False)]
 
         for _ in range(num_blocks):
             decoder.append(BasicDecBlock(hidden_dim, hidden_dim // 2))
             hidden_dim = hidden_dim // 2
+        decoder.append(nn.Conv2d(hidden_dim, hidden_dim, kernel_size=1,
+                                 stride=1, padding=0, bias=False))
+        decoder.append(nn.Tanh())
         decoder.append(nn.Conv2d(hidden_dim, out_channels, kernel_size=1,
                                  stride=1, padding=0, bias=False))
-        decoder.append(nn.Sigmoid())
 
-        self.encoder = nn.Sequential(*encoder)
         self.decoder = nn.Sequential(*decoder)
+
+
+# noinspection PyTypeChecker
+class AE(nn.Module):
+    def __init__(self, in_channels, input_dim, out_channels,
+                 latent_dim, img_size=64, num_blocks=5):
+        super().__init__()
+        self.encoder = Encoder(in_channels,
+                               input_dim, latent_dim,
+                               img_size, num_blocks)
+
+        self.decoder = Decoder(out_channels,
+                               self.encoder.spatial_size, self.encoder.hidden_dim,
+                               latent_dim, num_blocks)
 
     def forward(self, x):
         x = self.encoder(x)
