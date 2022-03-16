a6b2838801bdeb01e29598bd6d7a9ab794167efd
Anton Zubekhin <anton-zubehin@mail.ru>
2022-03-16 17:47:52 +0300

    changed architecture of AE

--- models/model.py
+++ models/model.py
@@ -9,228 +9,151 @@ from models.AE_init_weights import initialize_weights_normalCurve, initialize_we
 
 
 # noinspection PyTypeChecker
-class BasicEncBlock(nn.Module):
+class BasicBlock(nn.Module):
     def __init__(self, input_dim, output_dim, kernel_size=3, stride=1,
-                 padding=1, residual=True, nonlinearity=nn.LeakyReLU(0.2)):
+                 padding=1):
         super().__init__()
-        block = [
-            nn.Conv2d(input_dim, output_dim, kernel_size=kernel_size, stride=stride,
-                      padding=padding),
+        self.encode = nn.Sequential(
+            nn.Conv2d(input_dim, output_dim, kernel_size=kernel_size,
+                      stride=stride, padding=padding),
             nn.BatchNorm2d(output_dim),
-            nonlinearity
-        ]
-        self.residual = residual
-        self.encode = nn.Sequential(*block)
+            nn.LeakyReLU(0.2)
+        )
 
     def forward(self, x):
-        if self.residual:
-            x = x + self.encode(x)
-        else:
-            x = self.encode(x)
-        return x
+        out = self.encode(x)
+        return out
 
 
-class BasicEncBlockCurve(nn.Module):
+class BasicBlockCurve(nn.Module):
     def __init__(self, input_dim, output_dim, fix_points, kernel_size=3, stride=1,
-                 padding=1, residual=True, nonlinearity=nn.LeakyReLU(0.2)):
+                 padding=1):
         super().__init__()
         self.conv1 = curves.Conv2d(input_dim, output_dim, kernel_size=kernel_size, stride=stride,
                                    padding=padding, fix_points=fix_points)
         self.bn1 = curves.BatchNorm2d(output_dim, fix_points=fix_points)
-        self.nonlinearity = nonlinearity
-        self.residual = residual
+        self.nonlinearity = nn.LeakyReLU(0.2)
 
     def forward(self, x, coeffs_t):
         out = self.conv1(x, coeffs_t)
         out = self.bn1(out, coeffs_t)
         out = self.nonlinearity(out)
-        if self.residual:
-            out = x + out
         return out
 
 
 # noinspection PyTypeChecker
-class BasicDecBlock(nn.Module):
-    def __init__(self, input_dim, output_dim, kernel_size=3, stride=1,
-                 padding=0, nonlinearity=nn.LeakyReLU(0.2)):
+class Encoder(nn.Module):
+    def __init__(self, in_channels, latent_dim, input_dim=8, num_blocks=4):
         super().__init__()
-        block = [
-            nn.Upsample(scale_factor=2, mode='nearest'),
-            nn.ReplicationPad2d(1),
-            nn.Conv2d(input_dim, output_dim, kernel_size=kernel_size, stride=stride,
-                      padding=padding),
-            nn.BatchNorm2d(output_dim),
-            nonlinearity
-        ]
-        self.decode = nn.Sequential(*block)
+        self.maxpool = nn.MaxPool2d(2, 2)
+
+        self.encoder = nn.ModuleList()
+        self.encoder.append(BasicBlock(in_channels, input_dim))
+        hid_dim = input_dim
+        for _ in range(1, num_blocks):
+            self.encoder.append(BasicBlock(hid_dim, hid_dim * 2))
+            hid_dim *= 2
+
+        self.hidden_dim = hid_dim
+        self.final_conv = nn.Conv2d(hid_dim, latent_dim, kernel_size=4,
+                                    stride=1, padding=0, bias=False)
 
     def forward(self, x):
-        return self.decode(x)
-
-
-class BasicDecBlockCurve(nn.Module):
-    def __init__(self, input_dim, output_dim, fix_points, kernel_size=3, stride=1,
-                 padding=0, nonlinearity=nn.LeakyReLU(0.2)):
-        super().__init__()
-        self.upsample = nn.Upsample(scale_factor=2, mode='nearest')
-        self.replica = nn.ReplicationPad2d(1)
-        self.conv1 = curves.Conv2d(input_dim, output_dim, kernel_size=kernel_size, stride=stride,
-                                   padding=padding, fix_points=fix_points)
-        self.bn1 = curves.BatchNorm2d(output_dim, fix_points=fix_points)
-        self.nonlinearity = nonlinearity
-
-    def forward(self, x, coeffs_t):
-        x = self.upsample(x)
-        x = self.replica(x)
-        x = self.conv1(x, coeffs_t)
-        x = self.bn1(x, coeffs_t)
-        x = self.nonlinearity(x)
+        for block in self.encoder:
+            x = self.maxpool(block(x))
+        x = self.final_conv(x)
         return x
 
 
-# noinspection PyTypeChecker
-class Encoder(nn.Module):
-    def __init__(self, in_channels, input_dim,
-                 latent_dim, img_size=64, num_blocks=4):
-        super().__init__()
-        encoder = [nn.Conv2d(in_channels, input_dim, kernel_size=1,
-                             stride=1, padding=0, bias=False),
-                   nn.Tanh()]
-        hidden_dim = input_dim
-        spatial_size = img_size
-        for _ in range(num_blocks):
-            encoder.append(BasicEncBlock(hidden_dim, hidden_dim * 2, residual=False))
-            hidden_dim *= 2
-            if _ % 2 == 0:
-                encoder.append(BasicEncBlock(hidden_dim, hidden_dim, residual=True))
-            encoder.append(BasicEncBlock(hidden_dim, hidden_dim,
-                                         kernel_size=5, stride=2, padding=2,
-                                         residual=False))
-            spatial_size = spatial_size // 2
-        #encoder.append(nn.AvgPool2d(2))
-        encoder.append(nn.Conv2d(hidden_dim, hidden_dim, kernel_size=4,
-                                 stride=1, padding=0, bias=False))
-        encoder.append(nn.Conv2d(hidden_dim, latent_dim, kernel_size=1,
-                                 stride=1, padding=0, bias=False))
-        self.spatial_size = spatial_size
-        self.hidden_dim = hidden_dim
-        self.encoder = nn.Sequential(*encoder)
-
-    def forward(self, x):
-        return self.encoder(x)
-
-
 class EncoderCurve(nn.Module):
-    def __init__(self, in_channels, input_dim,
-                 latent_dim, fix_points, img_size=64, num_blocks=5):
+    def __init__(self, in_channels, latent_dim, fix_points, input_dim=8, num_blocks=4):
         super().__init__()
-        self.conv1 = curves.Conv2d(in_channels, input_dim, kernel_size=1, stride=1,
-                                   padding=0, bias=False, fix_points=fix_points)
-        hidden_dim = input_dim
-        spatial_size = img_size
-        self.hidden_dim = hidden_dim
-        self.spatial_size = spatial_size
-        self.layer = self._make_layer(BasicEncBlockCurve, num_blocks, fix_points)
-        self.conv2 = curves.Conv2d(hidden_dim, latent_dim, kernel_size=1, stride=1,
-                                   padding=0, bias=False, fix_points=fix_points)
-        self.flatten = nn.Flatten()
-        self.linear = curves.Linear(latent_dim * spatial_size * spatial_size,
-                                    latent_dim, bias=True, fix_points=fix_points)
+        self.maxpool = nn.MaxPool2d(2, 2)
+        self.conv1 = BasicBlockCurve(in_channels, input_dim, kernel_size=3, stride=1,
+                                     padding=1, fix_points=fix_points)
+        self.hidden_dim = input_dim
+        self.layer = self._make_layer(BasicBlockCurve, num_blocks, fix_points)
+        self.final_conv = curves.Conv2d(self.hidden_dim, latent_dim, kernel_size=4, stride=1,
+                                        padding=0, bias=False, fix_points=fix_points)
 
     def _make_layer(self, block, num_blocks, fix_points):
         layers = []
-        for _ in range(num_blocks):
+        for _ in range(1, num_blocks):
             layers.append(block(self.hidden_dim, self.hidden_dim * 2, fix_points))
             self.hidden_dim *= 2
-            self.spatial_size //= 2
         return nn.ModuleList(layers)
 
     def forward(self, x, coeffs_t):
-        x = self.conv1(x, coeffs_t)
+        x = self.maxpool(self.conv1(x, coeffs_t))
         for block in self.layer:
-            x = block(x, coeffs_t)
-        x = self.conv2(x, coeffs_t)
-        x = self.flatten(x)
-        x = self.linear(x)
+            x = self.maxpool(block(x, coeffs_t))
+        x = self.final_conv(x, coeffs_t)
         return x
 
 
 # noinspection PyTypeChecker
 class Decoder(nn.Module):
-    def __init__(self, out_channels, hidden_dim,
-                 latent_dim, num_blocks=4):
+    def __init__(self, out_channels, latent_dim, input_dim, num_blocks=4):
         super().__init__()
-        decoder = [nn.Conv2d(latent_dim, hidden_dim, kernel_size=1,
-                             stride=1, padding=0, bias=False),
-                   nn.Upsample(scale_factor=4, mode='nearest'),
-                   nn.Conv2d(hidden_dim, hidden_dim, kernel_size=1,
-                             stride=1, padding=0, bias=False)]
-
-        for _ in range(num_blocks):
-            decoder.append(BasicDecBlock(hidden_dim, hidden_dim))
-            if _ % 2 == 1:
-                decoder.append(BasicEncBlock(hidden_dim, hidden_dim, residual=True))
-            decoder.append(BasicEncBlock(hidden_dim, hidden_dim // 2, residual=False))
-            hidden_dim = hidden_dim // 2
-
-        decoder.append(nn.Conv2d(hidden_dim, hidden_dim, kernel_size=1,
-                                 stride=1, padding=0, bias=False))
-        decoder.append(nn.Tanh())
-        decoder.append(nn.Conv2d(hidden_dim, out_channels, kernel_size=1,
-                                 stride=1, padding=0, bias=False))
-        self.decoder = nn.Sequential(*decoder)
+        self.upsample = nn.Upsample(scale_factor=2, mode='nearest')
+        self.upblock = nn.Sequential(
+            nn.Upsample(scale_factor=4, mode='nearest'),
+            nn.Conv2d(latent_dim, input_dim, kernel_size=1, stride=1, padding=0, bias=False)
+        )
+        self.decoder = nn.ModuleList()
+        for _ in range(num_blocks-1):
+            self.decoder.append(BasicBlock(input_dim, input_dim // 2))
+            input_dim //= 2
+        self.decoder.append(BasicBlock(input_dim, out_channels))
+        self.final_conv = nn.Conv2d(out_channels, out_channels, kernel_size=1,
+                                    stride=1, padding=0, bias=False)
 
     def forward(self, x):
-        return self.decoder(x)
+        x = self.upblock(x)
+        for block in self.decoder:
+            x = self.upsample(block(x))
+        x = self.final_conv(x)
+        return x
 
 
 class DecoderCurve(nn.Module):
-    def __init__(self, out_channels, input_dim, hidden_dim,
-                 latent_dim, fix_points, num_blocks=5):
+    def __init__(self, out_channels, latent_dim, fix_points, input_dim, num_blocks=4):
         super().__init__()
-        self.linear = curves.Linear(latent_dim, latent_dim * input_dim * input_dim, fix_points)
-        self.unflatten = nn.Unflatten(1, (latent_dim, input_dim, input_dim))
-        self.conv1 = curves.Conv2d(latent_dim, hidden_dim, kernel_size=1, stride=1,
-                                   padding=0, bias=False, fix_points=fix_points)
-        self.hidden_dim = hidden_dim
-        self.layer = self._make_layer(BasicDecBlockCurve, num_blocks, fix_points)
-        self.conv2 = curves.Conv2d(hidden_dim, hidden_dim, kernel_size=1, stride=1,
-                                   padding=0, bias=False, fix_points=fix_points)
-        self.nonlinearity = nn.Tanh()
-        self.conv3 = curves.Conv2d(hidden_dim, out_channels, kernel_size=1, stride=1,
-                                   padding=0, bias=False, fix_points=fix_points)
+        self.upsample = nn.Upsample(scale_factor=2, mode='nearest')
+        self.hidden_dim = input_dim
+
+        self.upsample4 = nn.Upsample(scale_factor=4, mode='nearest')
+        self.upconv = curves.Conv2d(latent_dim, input_dim, kernel_size=1,
+                                    stride=1, padding=0, bias=False, fix_points=fix_points)
+
+        self.layer = self._make_layer(BasicBlockCurve, num_blocks, fix_points)
+        self.layer.appendc(BasicBlockCurve(self.hidden_dim, out_channels, fix_points))
+
+        self.final_conv = curves.Conv2d(out_channels, out_channels, kernel_size=1, stride=1,
+                                        padding=0, bias=False, fix_points=fix_points)
 
     def _make_layer(self, block, num_blocks, fix_points):
         layers = []
-        for _ in range(num_blocks):
+        for _ in range(num_blocks-1):
             layers.append(block(self.hidden_dim, self.hidden_dim // 2, fix_points))
             self.hidden_dim //= 2
         return nn.ModuleList(layers)
 
     def forward(self, x, coeffs_t):
-        x = self.linear(x, coeffs_t)
-        x = self.unflatten(x)
-        x = self.conv1(x)
+        x = self.upconv(self.upsample4(x))
         for block in self.layer:
-            x = block(x, coeffs_t)
-        x = self.conv2(x, coeffs_t)
-        x = self.nonlinearity(x)
-        x = self.linear(x)
-        x = self.conv3(x)
+            x = self.upsample(block(x))
+        x = self.final_conv(x)
         return x
 
 
 # noinspection PyTypeChecker
 class AEBase(nn.Module):
     def __init__(self, in_channels, input_dim, out_channels,
-                 latent_dim, conv_init='normal', img_size=64, num_blocks=4):
+                 latent_dim, conv_init='normal', num_blocks=4):
         super().__init__()
-        self.encoder = Encoder(in_channels,
-                               input_dim, latent_dim,
-                               img_size, num_blocks)
-        self.decoder = Decoder(out_channels,
-                               self.encoder.hidden_dim, latent_dim,
-                               num_blocks)
+        self.encoder = Encoder(in_channels, input_dim, latent_dim, num_blocks)
+        self.decoder = Decoder(out_channels, self.encoder.hidden_dim, latent_dim, num_blocks)
         for m in self.modules():
             if conv_init == 'normal':
                 initialize_weights_normal(m)
@@ -249,16 +172,13 @@ class AEBase(nn.Module):
 
 class AECurve(nn.Module):
     def __init__(self, in_channels, input_dim, out_channels,
-                 latent_dim, fix_points, conv_init='normal', img_size=64, num_blocks=5):
+                 latent_dim, fix_points, conv_init='normal', num_blocks=4):
         super().__init__()
-        self.encoder = EncoderCurve(in_channels,
-                                    input_dim, latent_dim, fix_points,
-                                    img_size, num_blocks)
+        self.encoder = EncoderCurve(in_channels, input_dim, latent_dim, fix_points,
+                                    num_blocks)
 
-        self.decoder = DecoderCurve(out_channels,
-                                    self.encoder.spatial_size, self.encoder.hidden_dim,
-                                    latent_dim, fix_points, num_blocks)
-
+        self.decoder = DecoderCurve(out_channels, self.encoder.hidden_dim, latent_dim, fix_points,
+                                    num_blocks)
         if conv_init == 'normal':
             self.encoder.apply(initialize_weights_normalCurve)
             self.decoder.apply(initialize_weights_normalCurve)
@@ -286,6 +206,5 @@ class AE:
         'latent_dim': 128,
         'out_channels': 3,
         'conv_init': 'normal',
-        'img_size': 64,
-        'num_blocks': 5
+        'num_blocks': 4
     }
