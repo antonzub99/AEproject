14912688a4dfa38bdc7e1f5f26f14ef271a7148a
poliik <43605977+poliik@users.noreply.github.com>
2022-03-20 10:28:12 +0300

    Add trivial connect, new names of files

--- eval_curve.py
+++ eval_curve.py
@@ -15,8 +15,14 @@ from lpips_pytorch import LPIPS
 
 import time
 
+parser = argparse.ArgumentParser(description='Connection evaluation')
 
-parser = argparse.ArgumentParser(description='AE curve evaluation')
+parser.add_argument('--connect', type=str, default=None, 
+                    help='trivial connect or curve[TRIVIAL/CURVE]')
+parser.add_argument('--start', type=int, default=None, 
+                    help='number of first checkpoint')
+parser.add_argument('--end', type=int, default=None, 
+                    help='number of second checkpoint')
 
 parser.add_argument('--dir', type=str, default='./tmp/eval', metavar='DIR',
                     help='training directory (default: ./tmp/eval)')
@@ -24,8 +30,6 @@ parser.add_argument('--device', type=str, default='cpu',
                     choices=['cpu', f"cuda:{0}"], help='device for calculations')
 parser.add_argument('--data_path', type=str, default='./data/', metavar='PATH',
                     help='path to datasets location (default: /data/)')
-parser.add_argument('--filename', type=str, default='curve.npz',
-                    help='filename of results file')
 
 parser.add_argument('--batch_size', type=int, default=64, metavar='N',
                     help='input batch size (default: 64)')
@@ -40,6 +44,10 @@ parser.add_argument('--num_bends', type=int, default=3, metavar='N',
                     help='number of curve bends (default: 3)')
 parser.add_argument('--ckpt', type=str, default=None, metavar='CKPT',
                     help='checkpoint to eval (default: None)')
+parser.add_argument('--init_start', type=str, default=None, metavar='CKPT',
+                    help='checkpoint to init start point (default: None)')
+parser.add_argument('--init_end', type=str, default=None, metavar='CKPT',
+                    help='checkpoint to init end point (default: None)')
 
 parser.add_argument('--num_points', type=int, default=61, metavar='N',
                     help='number of points on the curve (default: 61)')
@@ -66,6 +74,8 @@ def stats(values, dl):
     int_ = np.sum(0.5 * (values[:-1] + values[1:]) * dl[1:]) / np.sum(dl[1:])
     return min_, max_, avg, int_
 
+def get_weights(model):
+    return np.concatenate([p.data.cpu().numpy().ravel() for p in model.parameters()])
 
 def evaluate(args):
     os.makedirs(args.dir, exist_ok=True)
@@ -85,19 +95,32 @@ def evaluate(args):
         'nc': 3,
         'dropout': 0.05
         }
+    
+    if args.connect == 'CURVE':
+        curve = getattr(curves, args.curve)
+        model = curves.CurveNet(
+            curve,
+            autoencoders.CelebaAutoencoderCurve,
+            args.num_bends,
+            architecture_kwargs=kwargs,
+        )
+        model.to(args.device)
+        model.eval()
 
-    curve = getattr(curves, args.curve)
-    model = curves.CurveNet(
-        curve,
-        autoencoders.CelebaAutoencoderCurve,
-        args.num_bends,
-        architecture_kwargs=kwargs,
-    )
-    model.to(args.device)
-    model.eval()
+        checkpoint = torch.load(args.ckpt)
+        model.load_state_dict(checkpoint['model_state'])
+    elif args.connect == 'TRIVIAL':
+        model = autoencoders.CelebaAutoencoder(**kwargs)
+        model.to(args.device)
 
-    checkpoint = torch.load(args.ckpt)
-    model.load_state_dict(checkpoint['model_state'])
+        model.load_state_dict(torch.load(args.init_start)['model_state'])
+        w_1 = get_weights(model)
+        model.load_state_dict(torch.load(args.init_end)['model_state'])
+        w_2 = get_weights(model)
+    else:
+        raise NotImplementedError
+
+    regularizer = None if args.connect == 'TRIVIAL' else curves.l2_regularizer(args.wd)
 
     if args.loss_function == 'mae':
         criterion = torch.nn.L1Loss()
@@ -106,8 +129,6 @@ def evaluate(args):
                             device=args.device)
     else:
         raise NotImplementedError
-
-    regularizer = curves.l2_regularizer(args.wd)
 
     T = args.num_points
     ts = np.linspace(0.0, 1.0, T)
@@ -135,15 +156,27 @@ def evaluate(args):
     t = torch.FloatTensor([0.0]).to(args.device)
     for i, t_value in enumerate(ts):
         time_ep = time.perf_counter()
-        t.data.fill_(t_value)
-        weights = model.weights(t)
-        if previous_weights is not None:
-            dl[i] = np.sqrt(np.sum(np.square(weights - previous_weights)))
-        previous_weights = weights.copy()
 
-        utils.update_bn(loaders['train'], model, args.device, t=t)
-        train_res = trainer.test(loaders['train'], model, criterion, args.device, tboard, regularizer, t=t)
-        test_res = trainer.test(loaders['test'], model, criterion, args.device, tboard, regularizer, t=t)
+        kwargs_curve = {}
+        if args.connect=='TRIVIAL':
+            w = (1.0 - t_value) * w_1 + t_value * w_2
+            offset = 0
+            for parameter in model.parameters():
+                size = np.prod(parameter.size())
+                value = w[offset:offset+size].reshape(parameter.size())
+                parameter.data.copy_(torch.from_numpy(value))
+                offset += size
+        else:    
+            t.data.fill_(t_value)
+            kwargs_curve['t'] = t
+            weights = model.weights(t)
+            if previous_weights is not None:
+                dl[i] = np.sqrt(np.sum(np.square(weights - previous_weights)))
+            previous_weights = weights.copy()
+
+        utils.update_bn(loaders['train'], model, args.device, **kwargs_curve)
+        train_res = trainer.test(loaders['train'], model, criterion, args.device, tboard, regularizer, **kwargs_curve)
+        test_res = trainer.test(loaders['test'], model, criterion, args.device, tboard, regularizer, **kwargs_curve)
         train_loss[i] = train_res['loss']
         test_loss[i] = test_res['loss']
 
@@ -173,50 +206,59 @@ def evaluate(args):
         print(table)
 
         with torch.no_grad():
-            outp = model(eval_images, t=t)
+            outp = model(eval_images, **kwargs_curve)
             images_dynamics.append(outp.detach().cpu().numpy())
 
     if tboard is not None:
         tboard.close()
 
-    train_loss_min, train_loss_max, train_loss_avg, train_loss_int = stats(train_loss, dl)
-    test_loss_min, test_loss_max, test_loss_avg, test_loss_int = stats(test_loss, dl)
+    if args.connect=='CURVE':   
+        train_loss_min, train_loss_max, train_loss_avg, train_loss_int = stats(train_loss, dl)
+        test_loss_min, test_loss_max, test_loss_avg, test_loss_int = stats(test_loss, dl)
 
-    print('Length: %.2f' % np.sum(dl))
-    print(tabulate.tabulate([
-            ['train loss', train_loss[0], train_loss[-1], train_loss_min, train_loss_max, train_loss_avg, train_loss_int],
-            ['test loss', test_loss[0], test_loss[-1], test_loss_min, test_loss_max, test_loss_avg, test_loss_int],
-        ], [
-            '', 'start', 'end', 'min', 'max', 'avg', 'int'
-        ], tablefmt='simple', floatfmt='10.4f'))
+        print('Length: %.2f' % np.sum(dl))
+        print(tabulate.tabulate([
+                ['train loss', train_loss[0], train_loss[-1], train_loss_min, train_loss_max, train_loss_avg, train_loss_int],
+                ['test loss', test_loss[0], test_loss[-1], test_loss_min, test_loss_max, test_loss_avg, test_loss_int],
+            ], [
+                '', 'start', 'end', 'min', 'max', 'avg', 'int'
+            ], tablefmt='simple', floatfmt='10.4f'))
 
-    filepath = os.path.join(args.dir, args.filename)
+        filepath = os.path.join(args.dir, f'curve_stats{args.start}{args.end}.npz')
+        np.savez(
+            filepath,
+            ts=ts,
+            train_loss=train_loss,
+            train_loss_min=train_loss_min,
+            train_loss_max=train_loss_max,
+            train_loss_avg=train_loss_avg,
+            train_loss_int=train_loss_int,
+
+            test_loss=test_loss,
+            test_loss_min=test_loss_min,
+            test_loss_max=test_loss_max,
+            test_loss_avg=test_loss_avg,
+            test_loss_int=test_loss_int,
+        )
+
+    filepath = os.path.join(args.dir, f'losses{args.start}{args.end}.npz')
     np.savez(
         filepath,
         ts=ts,
         train_loss=train_loss,
-        train_loss_min=train_loss_min,
-        train_loss_max=train_loss_max,
-        train_loss_avg=train_loss_avg,
-        train_loss_int=train_loss_int,
-
-        test_loss=test_loss,
-        test_loss_min=test_loss_min,
-        test_loss_max=test_loss_max,
-        test_loss_avg=test_loss_avg,
-        test_loss_int=test_loss_int,
+        test_loss=test_loss
     )
 
     if args.lpips:
         lpips_stat = np.array(lpips_stat)
-        filepath = os.path.join(args.dir, 'lpipses.npz')
+        filepath = os.path.join(args.dir, f'lpipses{args.start}{args.end}.npz')
         np.savez(
             filepath,
             lpips=lpips_stat
         )
 
     images_dynamics = np.array(images_dynamics)
-    filepath = os.path.join(args.dir, 'images.npz')
+    filepath = os.path.join(args.dir, f'images{args.start}{args.end}.npz')
     np.savez(
         filepath,
         images_dynamics=images_dynamics
